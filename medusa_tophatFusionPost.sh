c247111e2e7dc1645015673b4430e9e8c5ff1b3c
Austin Christofferson <achristofferson@dback-login1.tgen.org>
2017-12-20 19:47:28 -0700

    optimized core count and memory on ThFpost so that it can run on 16 core nodes

--- medusa_tophatFusionPost.sh
+++ medusa_tophatFusionPost.sh
@@ -116,7 +116,7 @@ do
     fi
     #end linking stuff
 
-    sbatch -n 1 -N 1 --cpus-per-task $nCores --mem-per-cpu 10000 --output $runDir/oeFiles/%x-slurm-%j.out --export ALL,THFUSION2VCFPATH=$thFusion2vcfPath,TOPHAT2PATH=$tophat2Path,RUNDIR=$runDir,DIR=$topHatFDir,INDEXBASE=$indexbase,NXT1=$nxtStep1,REF=$ref,D=$d $pbsHome/medusa_tophatFusionPost.sh
+    sbatch -n 1 -N 1 --cpus-per-task $nCores --mem-per-cpu 3000 --output $runDir/oeFiles/%x-slurm-%j.out --export ALL,THFUSION2VCFPATH=$thFusion2vcfPath,TOPHAT2PATH=$tophat2Path,RUNDIR=$runDir,DIR=$topHatFDir,INDEXBASE=$indexbase,NXT1=$nxtStep1,REF=$ref,D=$d $pbsHome/medusa_tophatFusionPost.sh
     if [ $? -eq 0 ] ; then
         touch $topHatFDir.thFPostInQueue
     else
