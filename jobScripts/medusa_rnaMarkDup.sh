517591976aa05879c5950c7edc573e3b132cf830
Bryce Turner <40504111+TGEN-BTurner@users.noreply.github.com>
2019-09-24 10:53:39 -0700

    Update medusa_rnaMarkDup.sh
    Same as medusa_markDups.sh, 32G of mem is going to be assigned by default, so we allow Java to use 30G.

--- jobScripts/medusa_rnaMarkDup.sh
+++ jobScripts/medusa_rnaMarkDup.sh
@@ -14,7 +14,7 @@ echo "### BAMFILE: ${BAMFILE}"
 echo "### SAMTOOLSPATH: ${SAMTOOLSPATH}"
 
 echo "### Starting picard mark duplicates"
-perf stat /usr/lib/jvm/java-1.7.0-openjdk-1.7.0.91-2.6.2.3.el7.x86_64/jre/bin/java -Xmx22g -jar ${PICARDPATH}/MarkDuplicates.jar ASSUME_SORTED=true REMOVE_DUPLICATES=false VALIDATION_STRINGENCY=SILENT TMP_DIR=${TMPDIR} INPUT=${BAMFILE} OUTPUT=${OUTPUTBAM} METRICS_FILE=${BAMFILE}.picStats.MarkDupMetrics MAX_RECORDS_IN_RAM=18000000 CREATE_INDEX=true 2> ${BAMFILE}.markDups.perfOut > ${BAMFILE}.rnaMarkDupOut
+perf stat /usr/lib/jvm/java-1.7.0-openjdk-1.7.0.91-2.6.2.3.el7.x86_64/jre/bin/java -Xmx30g -jar ${PICARDPATH}/MarkDuplicates.jar ASSUME_SORTED=true REMOVE_DUPLICATES=false VALIDATION_STRINGENCY=SILENT TMP_DIR=${TMPDIR} INPUT=${BAMFILE} OUTPUT=${OUTPUTBAM} METRICS_FILE=${BAMFILE}.picStats.MarkDupMetrics MAX_RECORDS_IN_RAM=18000000 CREATE_INDEX=true 2> ${BAMFILE}.markDups.perfOut > ${BAMFILE}.rnaMarkDupOut
 if [ $? -eq 0 ] ; then
     mv ${BAMFILE}.rnaMarkDupOut ${BAMFILE}.rnaMarkDupPass
     #echo "Automatically removed by mark duplicates step to save on space" > ${BAMFILE}
